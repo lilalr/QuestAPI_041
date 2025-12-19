9664897e1251392c98adbaf329645ba1ff189a61
lilalr <gurennut@gmail.com>
2025-12-19 22:56:06 +0700

    menambah destinasi entry siswa

---
+++ app/src/main/java/com/example/questapi_041/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_041.view.route
+
+import com.example.questapi_041.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
