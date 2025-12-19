2a99d014e7bbb75a7ed16653cea99902ce8a0be6
lilalr <gurennut@gmail.com>
2025-12-19 22:56:18 +0700

    menambah destinasi home

---
+++ app/src/main/java/com/example/questapi_041/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_041.view.route
+
+import com.example.questapi_041.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
