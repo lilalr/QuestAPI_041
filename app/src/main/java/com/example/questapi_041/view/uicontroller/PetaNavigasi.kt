8007bf3e61128d2340f021eea26c23c3a8317ed0
lilalr <gurennut@gmail.com>
2025-12-19 22:58:52 +0700

    menambah navigasi home ke entry siswa

--- app/src/main/java/com/example/questapi_041/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_041/view/uicontroller/PetaNavigasi.kt
@@ -19,3 +19,33 @@ fun DataSiswaApp(
     HostNavigasi(navController = navController)
 }
 
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                navigateToItemUpdate = {
+                    // navController.navigate("${DestinasiDetail.route}/${it}")
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
