25cddf48fe71d6ed5eb26314d5c7360035aab59b
lilalr <gurennut@gmail.com>
2025-12-19 22:55:18 +0700

    membuat Repository Data Siswa

---
+++ app/src/main/java/com/example/questapi_041/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,15 @@
+package com.example.questapi_041.repositori
+
+import com.example.questapi_041.apiservice.ServiceApiSiswa
+import com.example.questapi_041.modeldata.DataSiswa
+
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
