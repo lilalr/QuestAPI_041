e4c6e94ba6bb0e39f1a2545ee14276ceb7301bdf
lilalr <gurennut@gmail.com>
2025-12-26 23:03:05 +0700

    menambah get utk cari satu siswa

--- app/src/main/java/com/example/questapi_041/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_041/apiservice/ServiceApiSiswa.kt
@@ -1,14 +1,21 @@
 package com.example.questapi_041.apiservice
 
 import com.example.questapi_041.modeldata.DataSiswa
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
-interface ServiceApiSiswa{
+interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
-    suspend fun  postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
+
+    @GET("baca1Teman.php")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 }
