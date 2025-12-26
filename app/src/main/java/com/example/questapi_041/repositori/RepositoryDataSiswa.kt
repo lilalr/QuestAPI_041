c379aabb69b6a6f12153f62bfadf5027b94cf2b6
lilalr <gurennut@gmail.com>
2025-12-26 23:11:10 +0700

    menambah kode suspen fun get satu siswa

--- app/src/main/java/com/example/questapi_041/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_041/repositori/RepositoryDataSiswa.kt
@@ -2,14 +2,17 @@ package com.example.questapi_041.repositori
 
 import com.example.questapi_041.apiservice.ServiceApiSiswa
 import com.example.questapi_041.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa{
-    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+
 }
+
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ):RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-}
