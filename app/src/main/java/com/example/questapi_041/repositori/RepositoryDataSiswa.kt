b40235e1606f9c129748e6748319b7f6b8f0c61a
lilalr <gurennut@gmail.com>
2025-12-26 23:11:33 +0700

    menambah kode suspend fun edit satu siswa

--- app/src/main/java/com/example/questapi_041/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_041/repositori/RepositoryDataSiswa.kt
@@ -8,6 +8,8 @@ interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
     suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+
 
 }
 
