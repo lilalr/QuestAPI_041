a92eaf4ef09c3182ff8b3a5ff7cfd4c8053b5415
lilalr <gurennut@gmail.com>
2025-12-26 23:19:24 +0700

    menambah initializer detail view model

--- app/src/main/java/com/example/questapi_041/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_041/viewmodel/provider/PenyediaViewModel.kt
@@ -1,24 +1,37 @@
 package com.example.questapi_041.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_041.repositori.AplikasiDataSiswa
+import com.example.questapi_041.viewmodel.DetailViewModel
+import com.example.questapi_041.viewmodel.EditViewModel
 import com.example.questapi_041.viewmodel.EntryViewModel
 import com.example.questapi_041.viewmodel.HomeViewModel
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
-        )
-
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as AplikasiDataSiswa)
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
         }
         initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
         }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
     }
 }
